9dbfb46385cafc499d4e66644c87cf0c20c2f76d
xstrex <strex@morphx.net>
2018-08-30 15:44:20 -0600

    added initial code

---
+++ mqtt-exec.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+MQTT_SERVER = "jabba.home.morphx.net"
+MQTT_PATH = "tablet"
+
+def on_connect(client, userdata, flags, rc):
+	print("Connected with result code "+str(rc))
+	client.subscribe(MQTT_PATH)
+
+def on_message(client, userdata, msg):
+	print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect(MQTT_SERVER, 1883, 60)
+
+client.loop_forever()
