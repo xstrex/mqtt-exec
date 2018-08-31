622754322241d177499b75ca2ae241d2b17cab71
xstrex <strex@morphx.net>
2018-08-30 19:30:48 -0600

    commented all prints

--- mqtt-exec.py
+++ mqtt-exec.py
@@ -9,37 +9,37 @@ STATUS_TOPIC = "tablet/tnix/status"
 CMD = "/home/pi/scripts/screen"
 
 def on_connect(client, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
+	# print("Connected with result code "+str(rc))
 	client.subscribe(SCREEN_TOPIC)
-	print("MQTT Server is: "+str(MQTT_SERVER))
-	print("Screen Control topic is: "+str(SCREEN_TOPIC))
-	print("Screen Status topic is: "+str(STATUS_TOPIC))
-	print("Command is: "+str(CMD))
+	# print("MQTT Server is: "+str(MQTT_SERVER))
+	# print("Screen Control topic is: "+str(SCREEN_TOPIC))
+	# print("Screen Status topic is: "+str(STATUS_TOPIC))
+	# print("Command is: "+str(CMD))
 	status()
 
 def screen_on():
-	print("Running screen on command")
+	# print("Running screen on command")
 	subprocess.call([CMD, "on"])
 	return
 
 def screen_off():
-	print("Running screen off command")
+	# print("Running screen off command")
 	subprocess.call([CMD, "off"])
 	return
 
 def clear():
-	print("Clearing retain flags on topic: "+str(SCREEN_TOPIC))
+	# print("Clearing retain flags on topic: "+str(SCREEN_TOPIC))
 	client.publish(SCREEN_TOPIC, None, 1, True);
 	return
 
 def status():
 	STAT_OUT = subprocess.check_output([CMD, "status"])
-	print("Screen is currently: "+str(STAT_OUT))
+	# print("Screen is currently: "+str(STAT_OUT))
 	client.publish(STATUS_TOPIC, STAT_OUT, qos=0, retain=False)
 	return
 
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
+	# print(msg.topic+" "+str(msg.payload))
 	if msg.payload == 'on':
 		screen_on()
 		status()
