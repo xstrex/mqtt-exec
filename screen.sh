6c5524f632c985e2b9d2767da6c82348f7251d04
xstrex <strex@morphx.net>
2019-03-11 15:42:02 -0600

    added arg check

--- screen.sh
+++ screen.sh
@@ -39,22 +39,26 @@ dim () {
 }
 
 brightness () {
-	if [ -n "$1" ] && [ "$1" -eq "$1" ] 2>/dev/null; then
-		if [[ "$1" -ge "$min_br" && "$1" -le "$max_br" ]]; then
-			echo "$1" | sudo tee /sys/class/backlight/rpi_backlight/brightness >/dev/null 2>&1
+	if [[ $# -le 1 ]]; then
+		if [[ -n "$1" ]] && [[ "$1" -eq "$1" ]] 2>/dev/null; then
+			if [[ "$1" -ge "$min_br" && "$1" -le "$max_br" ]]; then
+				echo "$1" | sudo tee /sys/class/backlight/rpi_backlight/brightness >/dev/null 2>&1
+			else
+				echo "Sorry, outside acceptable range"
+				usage
+			fi
 		else
-			echo "Sorry, outside acceptable range"
-			usage
+			echo "$statb"
 		fi
 	else
-		echo "$statb"
+		usage
 	fi
 }
 
 status () {
-	if [ "$stat" == 0 ]; then
+	if [[ "$stat" == 0 ]]; then
 		echo "on"
-	elif [ "$stat" == 1 ]; then
+	elif [[ "$stat" == 1 ]]; then
 		echo "off"
 	fi
 }
