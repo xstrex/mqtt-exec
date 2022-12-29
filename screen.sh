a740397489fded82f28536ddc1d3fa48efbca2e4
strex <strex@morphx.net>
2022-12-29 15:58:17 -0700

    fixed paths

--- screen.sh
+++ screen.sh
@@ -2,17 +2,21 @@
 
 ############################################################################
 # Purpose: Control the backlight on a 7" Raspberry Pi Screen
-# 
+#
 # Author: Strex
 # Contact: strex@morphx.net
-# Version: 0.1
-# Date: 3/11/19
+# Version: 0.2
+# Date: 12/29/22
 ############################################################################
+
+prefix=/sys/class/backlight/10-0045/
+bl_power="$prefix/bl_power"
+brightness="$prefix/brightness"
 
 ###############
 # Variables
-stat=$(cat /sys/class/backlight/rpi_backlight/bl_power)
-statb=$(cat /sys/class/backlight/rpi_backlight/brightness)
+stat=$(cat $bl_power)
+statb=$(cat $brightness)
 min_br="15"
 max_br="220"
 br="100"
@@ -25,25 +29,25 @@ usage () {
 }
 
 on () {
-	echo 0 | sudo tee /sys/class/backlight/rpi_backlight/bl_power >/dev/null 2>&1
+	echo 0 | sudo tee "$bl_power" >/dev/null 2>&1
 }
 
 off () {
-	echo 1 | sudo tee /sys/class/backlight/rpi_backlight/bl_power >/dev/null 2>&1
+	echo 1 | sudo tee "$bl_power" >/dev/null 2>&1
 }
 
 bright () {
-	echo "$br" | sudo tee /sys/class/backlight/rpi_backlight/brightness >/dev/null 2>&1
+	echo "$br" | sudo tee "$brightness" >/dev/null 2>&1
 }
 
 dim () {
-	echo "$dm" | sudo tee /sys/class/backlight/rpi_backlight/brightness >/dev/null 2>&1
+	echo "$dm" | sudo tee "$brightness" >/dev/null 2>&1
 }
 
 brightness () {
 	if [[ -n "$1" ]] && [[ "$1" -eq "$1" ]] 2>/dev/null; then
 		if [[ "$1" -ge "$min_br" && "$1" -le "$max_br" ]]; then
-			echo "$1" | sudo tee /sys/class/backlight/rpi_backlight/brightness >/dev/null 2>&1
+			echo "$1" | sudo tee "$brightness" >/dev/null 2>&1
 		else
 			echo "Sorry, outside acceptable range, or not a number"
 			usage
